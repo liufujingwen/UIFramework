abb62f53e7e6033b2ef3b11f9f22d896b050e2db
liufujingwen <394074705@qq.com>
2019-03-15 17:49:10 +0800

    UI框架

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -4,62 +4,56 @@ using System.Collections.Generic;
 using XLua;
 using System;
 using System.IO;
+using UIFramework;
 
-namespace UIFramework
+public class Main : MonoBehaviour
 {
-    public class Main : MonoBehaviour
+    public static Main Instance = null;
+    public LuaEnv LuaEnv = new LuaEnv();
+    string luaPath = "LuaScripts";
+
+    public void Start()
     {
-        public static Main Instance = null;
-        public LuaEnv LuaEnv = new LuaEnv();
-        string luaPath = "LuaScripts";
+        Instance = this;
 
-        public void Start()
-        {
-            Instance = this;
+        //lua路径
+        luaPath = Path.Combine(Directory.GetParent(Application.dataPath).FullName, luaPath);
+        LuaEnv.AddLoader(CustomLoader);
 
-            //lua路径
-            luaPath = Path.Combine(Directory.GetParent(Application.dataPath).FullName, luaPath);
-            LuaEnv.AddLoader(CustomLoader);
+        Import("Common");
+        Import("Manager");
+        Import("UI");
 
-            Import("Common");
-            Import("Manager");
-            Import("UI");
-
-            DllHelper.Init();
-            UIManager.Instance.Init();
-            UIManager.Instance.Push("MainMenuUI");
-        }
-
-        private byte[] CustomLoader(ref string filepath)
-        {
-            byte[] bytes = null;
-            string path = Path.Combine(luaPath, filepath + ".lua");
-            if (File.Exists(path))
-            {
-                bytes = File.ReadAllBytes(path);
-            }
-            return bytes;
-        }
-
-        public void Import(string path)
-        {
-            string importPath = Path.Combine(luaPath, path);
-            string[] luaFils = Directory.GetFiles(importPath, "*.lua", SearchOption.AllDirectories);
-            if (luaFils != null && luaFils.Length > 0)
-            {
-                for (int i = 0; i < luaFils.Length; i++)
-                {
-                    string luaFile = luaFils[i];
-                    byte[] bytes = File.ReadAllBytes(luaFile);
-                    LuaEnv.DoString(bytes, luaFile);
-                }
-            }
-        }
-
-        private void OnDestroy()
-        {
-            
-        }
-
+        DllHelper.Init();
+        UIManager.Instance.Init();
+        UIManager.Instance.Push("MainMenuUI");
     }
+
+    private byte[] CustomLoader(ref string filepath)
+    {
+        byte[] bytes = null;
+        string path = Path.Combine(luaPath, filepath + ".lua");
+        if (File.Exists(path))
+        {
+            bytes = File.ReadAllBytes(path);
+        }
+        return bytes;
+    }
+
+    public void Import(string path)
+    {
+        string importPath = Path.Combine(luaPath, path);
+        string[] luaFils = Directory.GetFiles(importPath, "*.lua", SearchOption.AllDirectories);
+        if (luaFils != null && luaFils.Length > 0)
+        {
+            for (int i = 0; i < luaFils.Length; i++)
+            {
+                string luaFile = luaFils[i];
+                byte[] bytes = File.ReadAllBytes(luaFile);
+                LuaEnv.DoString(bytes, luaFile);
+            }
+        }
+    }
+
 }
+
---
+++ Assets/UIFramework/Event/LuaEventProxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class LuaEventProxy : IBaseEventListener
+{
+    Action<string, object[]> luaAction = null;
+
+    public LuaEventProxy(Action<string, object[]> action)
+    {
+        this.luaAction = action;
+    }
+
+    public void OnNotifiy(string evt, params object[] args)
+    {
+        this.luaAction?.Invoke(evt, args);
+    }
+}
+
